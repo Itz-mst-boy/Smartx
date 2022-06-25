3e73768958c37eab4acc3005e1090556676c43b5
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-06-26 04:13:21 +0530

    Update Mukesh.py

--- MukeshRobot/modules/Mukesh.py
+++ MukeshRobot/modules/Mukesh.py
@@ -2,8 +2,11 @@ import asyncio
 from pyrogram import filters
 from pyrogram.types import (InlineKeyboardButton,                         InlineKeyboardMarkup, InputMediaPhoto, Message)
 
+
 from MukeshRobot import pbot as bot
+
 Mukesh = "https://telegra.ph/file/141146027deaaefbf7f5a.jpg"
+
 @bot.on_message(filters.command(["noob", "owner", "mukesh"]))
 async def repo(client, message):
 await message.reply_photo(
